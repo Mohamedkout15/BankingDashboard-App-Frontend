d85271767b2bff356677e41314fb3919ca9d65b1
Mohamedkout15 <mohamed.kout@outlook.com>
2024-01-31 03:33:59 +0100

    update

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { ClientService } from '../../services/Client.service';
 import { ClientDataService } from '../../services/clientdata.service';
 import { Subscription } from 'rxjs';
 import { Client } from '../../Models/Client.model';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,61 +12,72 @@ import { Client } from '../../Models/Client.model';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-  client: Client;
-  clientId: string;
-  private routeSubscription: Subscription;
-  selectedDate1: string; // To store selected date for the 1st visit
-  selectedDate2: string; // To store selected date for the 2nd visit
+    client: Client;
+    clientId: string;
+    private routeSubscription: Subscription;
+    selectedDate1: string; // To store selected date for the 1st visit
+    selectedDate2: string;
+    editDate1 = false; // Add this property
+    editDate2 = false;
 
-  constructor(
-      private clientService: ClientService,
-      private route: ActivatedRoute,
-      private clientDataService: ClientDataService
-  ) {}
+    constructor(
+        private clientService: ClientService,
+        private route: ActivatedRoute,
+        private clientDataService: ClientDataService
+    ) {
+    }
 
-  ngOnInit() {
-    this.routeSubscription = this.clientDataService.currentClientId.subscribe(
-        (id) => {
-          this.clientId = id;
-          this.fetchClientData();
-        }
-    );
-  }
+    ngOnInit() {
+        this.routeSubscription = this.clientDataService.currentClientId.subscribe(
+            (id) => {
+                this.clientId = id;
+                this.fetchClientData();
+            }
+        );
+    }
 
-  ngOnDestroy() {
-    this.routeSubscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.routeSubscription.unsubscribe();
+    }
 
-  fetchClientData() {
-    this.clientService.getClientById(this.clientId).subscribe(
-        (data: Client) => {
-          this.client = data;
-          console.log(this.client);
-        },
-        (error) => {
-          console.error('Error fetching client data', error);
-        }
-    );
-  }
+    fetchClientData() {
+        this.clientService.getClientById(this.clientId).subscribe(
+            (data: Client) => {
+                this.client = data;
+                console.log(this.client);
+            },
+            (error) => {
+                console.error('Error fetching client data', error);
+            }
+        );
+    }
 
-  setDatedxvisite(clientId: string, selectedDate: string) {
-    this.clientService.setDatedxvisite(clientId, new Date(selectedDate)).subscribe(
-        (data: Client) => {
-          this.client = data;
-        },
-        (error) => {
-          console.error('Error setting date', error);
-        }
-    );
-  }
+    setDatedxvisite(clientId: string, selectedDate: string) {
+        this.clientService.setDatedxvisite(clientId, new Date(selectedDate)).subscribe(
+            (data: Client) => {
+                this.client = data;
+            },
+            (error) => {
+                console.error('Error setting date', error);
+                if (error instanceof HttpErrorResponse) {
+                    console.error(`Status: ${error.status}, ${error.statusText}`);
+                }
+            }
+        );
+    }
+
     setDatePrvisite(clientId: string, selectedDate: string) {
-      this.clientService.setDatePrvisite(clientId, new Date(selectedDate)).subscribe(
-          (data: Client) => {
-              this.client = data ;
-          },
-          error => {
-              console.error('Error setting date', error);
-          }
-      );
-  }
+        this.clientService.setDatePrvisite(this.clientId, new Date(selectedDate)).subscribe(
+            (data: Client) => {
+                // Update the client data if needed
+                this.client = data;
+            },
+            (error) => {
+                console.error('Error setting date', error);
+                if (error instanceof HttpErrorResponse) {
+                    console.error(`Status: ${error.status}, ${error.statusText}`);
+                }
+            }
+        );
+    }
 }
